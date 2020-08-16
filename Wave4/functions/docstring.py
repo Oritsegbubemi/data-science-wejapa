139706d3d8a0452d92786d6bca18b94742c0a411
Gbubemi Makpokpomi <gbubemimakpokpomi@gmail.com>
2020-08-16 07:56:45 +0100

    added wave4 functions

---
+++ Wave4/functions/docstring.py
@@ -0,0 +1,8 @@
+def readable_timedelta(days):
+    """
+
+    """
+
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
---
+++ Wave4/functions/main.py
@@ -0,0 +1,14 @@
+print("Pop density")
+import pop_density
+
+print("\nStudent")
+import student
+
+print("\nDocstring")
+import docstring
+
+print("\nLambda Map")
+import lambda_map
+
+print("\nLambda Filter")
+import lambda_filter
